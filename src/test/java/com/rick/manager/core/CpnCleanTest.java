1cda786e4452bb51e45e1e4856c2754b52ebe913
rick <jkxyx205@163.com>
2024-09-20 11:50:33 +0800

    chore: form cpn 清理

--- src/test/java/com/rick/manager/core/CpnCleanTest.java
+++ src/test/java/com/rick/manager/core/CpnCleanTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 /**
  * @author Rick.Xu
  * @date 2024/9/14 17:17
@@ -18,7 +20,24 @@ public class CpnCleanTest {
 
     @Test
     public void testClean() {
-        String deletedIds = sharpService.queryForObject("select GROUP_CONCAT(id) ids from sys_form_configurer where not exists (select 1 from sys_form_cpn_configurer where sys_form_cpn_configurer.config_id = sys_form_configurer.id)", null, String.class).get();
-        SQLUtils.delete("sys_form_configurer", "id", deletedIds);
+        while (true) {
+            Optional<String> optional = sharpService.queryForObject("select GROUP_CONCAT(id) ids from sys_form_cpn_configurer where not exists (select 1 from sys_form where sys_form.id = sys_form_cpn_configurer.form_id)", null, String.class);
+            if (optional.isPresent()) {
+                String deletedIds = optional.get();
+                System.out.println("sys_form_cpn_configurer delete count = " + SQLUtils.delete("sys_form_cpn_configurer", "id", deletedIds));
+            } else {
+                break;
+            }
+        }
+
+        while (true) {
+            Optional<String> optional = sharpService.queryForObject("select GROUP_CONCAT(id) ids from sys_form_configurer where not exists (select 1 from sys_form_cpn_configurer where sys_form_cpn_configurer.config_id = sys_form_configurer.id)", null, String.class);
+            if (optional.isPresent()) {
+                String deletedIds = optional.get();
+                System.out.println("sys_form_configurer delete count = " + SQLUtils.delete("sys_form_configurer", "id", deletedIds));
+            } else {
+                break;
+            }
+        }
     }
 }
