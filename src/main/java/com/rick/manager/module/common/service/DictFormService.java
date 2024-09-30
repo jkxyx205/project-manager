faeda631cf9f269ad80834eab7860920f49a0fb2
rick <jkxyx205@163.com>
2024-09-30 17:52:40 +0800

    fix: 维护分类的时候，清空 form 缓存

--- src/main/java/com/rick/manager/module/common/service/DictFormService.java
+++ src/main/java/com/rick/manager/module/common/service/DictFormService.java
@@ -1,12 +1,14 @@
 package com.rick.manager.module.common.service;
 
 import com.rick.formflow.form.service.FormAdvice;
+import com.rick.formflow.form.service.FormUtils;
 import com.rick.formflow.form.service.bo.FormBO;
 import com.rick.meta.dict.service.DictService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Rick.Xu
@@ -19,7 +21,17 @@ public class DictFormService implements FormAdvice {
     private final DictService dictService;
 
     @Override
+    public void beforeInstanceHandle(FormBO form, Long instanceId, Map<String, Object> values) {
+        values.put("name", String.valueOf(values.get("name")).toUpperCase());
+    }
+
+    @Override
     public void afterInstanceHandle(FormBO form, Long instanceId, Map<String, Object> values) {
         dictService.rebuild((String) values.get("type"));
+        dictService.rebuild("sys_dict_category");
+
+        if (Objects.equals(values.get("type"), "PRODUCT_TYPE")) {
+            FormUtils.update(864275104153022464L, null);
+        }
     }
 }
